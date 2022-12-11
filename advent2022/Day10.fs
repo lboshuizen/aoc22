e3678c6d28b8d65cb52e168026c283c3a94734fe
Leon Boshuizen <llc.boshuizen@gmail.com>
2022-12-11 02:11:26 +0100

    ref

--- advent2022/Day10.fs
+++ advent2022/Day10.fs
@@ -12,13 +12,14 @@ let cycle (x,c) = function
                   | Noop -> (x,c @ [x])
                   | Add n -> (x+n,c @ [x;x+n])
                   
-let run = List.fold cycle (1,[1]) >> snd 
+let run = List.fold cycle (1,[1]) >> snd
+
 let part1 =
     let pick (xs:int list) = List.map (fun c -> c*xs[c-1]) [20..40..220]
     run >> pick >> List.sum
 
 let part2 =
     let draw i x = if i >= (x-1) && i <= (x+1) then '#' else '.'
-    run >> List.chunkBySize 40 >> List.map (List.mapi draw ) >> List.map toString
+    run >> List.chunkBySize 40 >> List.map (List.mapi draw >> toString)  
 
 let Solve (xs:string list) = xs |> parse |> both part1 part2

8730eadb11bbc5e50fb119bf91046d5e6aaa3661
Leon Boshuizen <llc.boshuizen@gmail.com>
2022-12-15 17:30:24 +0100

    refactored day15 to do rows in parallel

--- advent2022/Day15.fs
+++ advent2022/Day15.fs
@@ -1,8 +1,11 @@
 module Day15
 
+open FSharp.Collections.ParallelSeq
+
 let dim = Seq.map (fun ((x,_),d) -> (x-d),(x+d)) >> both (Seq.minBy fst >> fst) (Seq.maxBy snd >> snd)
 
-let parse = List.map (parseRegex "(-?\d+).*=(-?\d+).*=(-?\d+).*=(-?\d+)$" (fun a -> (int a[0],int a[1]),(int a[2],int a[3])) >> fun (p1,p2) -> (p1,manhattan p1 p2) )
+let parse = List.map (parseRegex "(-?\d+).*=(-?\d+).*=(-?\d+).*=(-?\d+)$"
+                     (fun a -> (int a[0],int a[1]),(int a[2],int a[3])) >> fun (s,b) -> (s,manhattan s b) )
             >> both id dim
 
 let exclude r (a,b) =
@@ -11,25 +14,30 @@ let exclude r (a,b) =
     | x,x' when x <= a && x' >= b -> []
     | x,x' when x > a && x' < b -> [(a,x-1);(x'+1,b)]
     | _,x' when x' < b -> [(x'+1,b)]
-    | x,_ when x > a -> [(a,x-1)]
+    | x,_  when x > a ->  [(a,x-1)]
     | _ -> failwith "missed case"
     
-let notCovered (mn,mx) row =
-    let cover row ((x,y),d) = let dy = d-abs(row-y)
-                              (x-dy,x+dy)
+let notCovered clip row =
+    let cover ((x,y),d) = let dy = d-abs(row-y)
+                          (x-dy,x+dy)
     let remove range = List.map (exclude range) >> List.concat
+    let sensorCoversRow ((_,y),d) = (abs (row-y)) <= d
     
-    List.filter (fun ((_,y),d) -> (abs (row-y)) <= d) >> List.map (cover row)
-    >> List.fold (flip remove) [(mn,mx)]
-    >> fun xs -> (row,xs)
+    List.filter sensorCoversRow >> List.map cover
+    >> List.fold (flip remove) [clip]
+    >> function
+       | [] -> None
+       | xs -> Some (row,xs) 
     
-let part1 (sensors,(mn,mx)) =
-    sensors |> (notCovered (mn,mx)) 2_000_000
-    |> snd |> List.pairwise |> List.map (fun ((_,a),(b,_)) -> b-a) |> List.sum
+let part1 (sensors,clip) =
+    sensors |> (notCovered clip) 2_000_000
+    |> Option.get |> snd |> List.pairwise |> List.map (fun ((_,a),(b,_)) -> b-a) |> List.sum
 
 let part2 (sensors,_) =
     let unwrap (y,xs) = xs |> List.head |> (fst >> int64), int64 y
-    let tune = List.head >> unwrap >> fun (x,y) -> x * 4_000_000L + y
-    [0..4_000_000] |> List.map (flip (notCovered (0,4_000_000)) sensors) |> List.filter (snd >> (<>) []) |> tune
+    let tune = Seq.choose id >> Seq.head >> unwrap
+    
+    let x,y = [0..4_000_000] |> PSeq.map (flip (notCovered (0, 4_000_000)) sensors) |> tune
+    x * 4_000_000L + y
 
-let Solve  = parse >> both part1 part2 >> shouldBe 4_985_195 11_583_882_601_918L
+let Solve = parse >> both part1 part2 >> shouldBe 4_985_195 11_583_882_601_918L

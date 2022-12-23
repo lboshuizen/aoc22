6361778ae21c359bf96bc5c432e7160ef98a2b54
Leon Boshuizen <llc.boshuizen@gmail.com>
2022-12-23 19:13:30 +0100

    fixed typo

--- advent2022/Day23.fs
+++ advent2022/Day23.fs
@@ -19,9 +19,10 @@ let parse = List.map List.ofSeq >> toGrid2d >> List.filter (snd >> (=) '#') >> L
 
 let onRow y = Seq.exists (snd >> (=) y) >> not
 let onCol x = Seq.exists (fst >> (=) x) >> not
+
 let look = [(pred,pred);(id,pred);(succ,pred);(pred,id);(succ,id);(pred,succ);(id,succ);(succ,succ)]
-let directions = [snd >> pred >> onRow;snd >> succ >> onRow;fst >> pred >>onCol;fst >> succ >> onCol]
-let moves = [(<!>) (id,pred);(<!>) (id,succ);(<!>) (pred,id);(<!>) (succ,id)]
+let props = [snd >> pred >> onRow;snd >> succ >> onRow;fst >> pred >>onCol;fst >> succ >> onCol] // N,S,W,E
+let moves = [(<!>)(id,pred);(<!>)(id,succ);(<!>)(pred,id);(<!>)(succ,id)]
 
 let propose (g,d,_) p = let around = List.map (flip (<!>) p) look |> List.filter (flip  Set.contains g)
                         let prop (x,y) xs = d |> List.map ((<*>) (x,y) >> (<*>) xs) |> Seq.tryFindIndex ((=) true)
@@ -35,18 +36,16 @@ let duplicates = PSeq.groupBy snd >> Seq.filter (snd >> Seq.length >> flip (>) 1
 
 let collided xs = xs |> List.partition (snd >> flip Set.contains (duplicates xs))
 
-let round (g,ds,mv) = let mvrs,stay = g |> PSeq.map (project (g,ds,mv)) |> List.ofSeq |> List.partition (snd >> Option.isSome)
-                      let col,ok = mvrs |> List.map (mapSnd Option.get) |> collided
-                      (List.map fst stay) @ (List.map fst col) @ (List.map snd ok) |> Set, rotate ds, rotate mv
+let round (g,ds,mv) = let move,idle = g |> PSeq.map (project (g,ds,mv)) |> List.ofSeq |> List.partition (snd >> Option.isSome)
+                      let col,ok = move |> List.map (mapSnd Option.get) |> collided
+                      (List.map fst idle) @ (List.map fst col) @ (List.map snd ok) |> Set, rotate ds, rotate mv
 
-let area s = let xs = Set.toList s
-             let x,x' = xs |> List.map fst |> both List.min List.max
-             let y,y' = xs |> List.map snd |> both List.min List.max
+let area s = let (x,x'),(y,y') = s |> both (Seq.map fst >> both Seq.min Seq.max) (Seq.map snd >> both Seq.min Seq.max)
              (x'-x+1)*(y'-y+1)
 
-let part1 s = times 10 round (s,directions,moves) |> fst3 |> both id (area) |> fun (s,b) -> b - Set.count s
+let part1 s = times 10 round (s,props,moves) |> fst3 |> both id area |> fun (s,a) -> a - Set.count s
 
 let part2 s = let next ((_,i),(s,ds,mv)) = (s,succ i),round (s,ds,mv)
-              until (fun ((s,_),(s',_,_)) -> s'=s) next ((Set.empty,0),(s,directions,moves)) |> fst |> snd                         
+              until (fun ((s,_),(s',_,_)) -> s'=s) next ((Set.empty,0),(s,props,moves)) |> fst |> snd                         
 
 let Solve : Solver<int> = parse >> both part1 part2 >> shouldBe 3996 908
